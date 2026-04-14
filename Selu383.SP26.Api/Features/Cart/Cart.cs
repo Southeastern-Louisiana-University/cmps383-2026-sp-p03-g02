4243b5c8d30d2eebf30620af49203da99bdf99ce
Jonathan Freeman <jonathan.freeman@selu.edu>
2026-04-13 19:57:32 -0500

    started on cart backend

---
+++ Selu383.SP26.Api/Features/Cart/Cart.cs
@@ -0,0 +1,15 @@
+﻿using Selu383.SP26.Api.Features.Orders;
+
+namespace Selu383.SP26.Api.Features.Cart
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public bool IsOrdered { get; set; }
+        public decimal Total { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public virtual ICollection<CartItem> CartItem { get; set; } = new List<CartItem>();
+    }
+}
